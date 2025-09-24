624ebd2c85db69e7a4f86d8244feb1235e5836cf
Cristian Chiorescu <cristian.chiores@gmail.com>
2025-09-23 22:23:40 -0600

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const fontPoppins = Poppins({
   variable: "--font-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
