afd3589652d8fb120775dbe324f405147ef159a6
Cristian Chiorescu <cristian.chiores@gmail.com>
2025-10-13 14:01:42 -0600

    fix: improve LCP

--- src/components/recipe-grid.tsx
+++ src/components/recipe-grid.tsx
@@ -105,21 +105,12 @@ export default function RecipeGrid({
                   />
                 </div>
               ) : (
-                <motion.div
-                  layout
-                  initial={{ opacity: 0, scale: 0.8 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  exit={{ opacity: 0, scale: 0.8 }}
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="h-full"
-                >
-                  <RecipeCard
-                    recipe={recipe}
-                    loading="lazy"
-                    fetchPriority="low"
-                    lowRes
-                  />
-                </motion.div>
+                <RecipeCard
+                  recipe={recipe}
+                  loading="lazy"
+                  fetchPriority="low"
+                  lowRes
+                />
               )}
             </Link>
           );
